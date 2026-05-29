e0df0ad5b45271c4e45a074c1f4fce27c4a0af59
josemarVicente <josemaralessio@gmail.com>
2026-05-29 16:05:07 +0100

    feat(config): add api url to config

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,12 @@
 const nextConfig = {
   async rewrites() {
+    const backendUrl =
+      process.env.NEXT_PUBLIC_API_URL || 'http://127.0.0.1:8000';
+
     return [
       {
         source: '/backend/:path*',
-        destination: 'http://127.0.0.1:8000/:path*',
+        destination: `${backendUrl}/:path*`,
       },
     ];
   },
